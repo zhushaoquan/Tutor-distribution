2cf6d877761472b4a485bcf74221253da376f48e
Liam <282129796@qq.com>
2016-11-07 04:53:47 +0800

    学生个人信息界面
    学生个人信息界面修改完成

--- tutorAssign/app/index/controller/Student.php
+++ tutorAssign/app/index/controller/Student.php
@@ -8,8 +8,13 @@ class Student extends BaseController {
 
 	public function index() {
 		$user = $this->auto_login();
-        
-        $this->assign('user', $user);
+		$student = Db::table('user_student')->where('serialNum',$user['serialNum'])->find(); //如果直接使用session里的用户信息，修改的信息必须重新登录才能更新显示
+        if ($user['chosen'] == 0) {
+        	$student['chosen'] = '否';
+        } else {
+        	$student['chosen'] = '是';
+        }
+        $this->assign('user', $student);
 		return $this->fetch('index');
 	}
 
@@ -18,5 +23,17 @@ class Student extends BaseController {
         
         $this->assign('user', $user);
 		return $this->fetch('tutorList');
+	}
+
+	public function modify() {
+		$user = $this->auto_login();
+		$student = Db::table('user_student')->where('serialNum',$user['serialNum'])->find(); //如果直接使用session里的用户信息，修改的信息必须重新登录才能更新显示
+        if ($user['chosen'] == 0) {
+        	$student['chosen'] = '否';
+        } else {
+        	$student['chosen'] = '是';
+        }
+        $this->assign('user', $student);
+		return $this->fetch('modify');
 	}
 }
