ab323161f4d72b12fe2a17a93a4be0a09011c8d8
tingting <771539035@qq.com>
2016-11-11 11:00:29 +0800

    修改TeacherTutor/index,

--- tutorAssign/app/index/controller/TeacherTutor.php
+++ tutorAssign/app/index/controller/TeacherTutor.php
@@ -8,13 +8,53 @@ class TeacherTutor extends BaseController {
 	
 	public function index() {
 		$user = $this->auto_login();
-		$teacher = Db::table('user_teacher')->where('workNumber',$user['workNumber'])->find();
+		$teacher = Db::table('tc_teacher')->where('workNumber',$user['workNumber'])->find();
+		$user = $teacher;
+/*		
+
+//wtt修改
+
 		if ($user['isExperial'] == 0) {
 			$teacher['isExperial'] = '否';
 		} else {
 			$teacher['isExperial'] = '是';
 		}
+*/
 		$this->assign('user', $teacher);
 		return $this->fetch('index');
+	}
+    
+    public function issueSubmit() {
+    	 $user = $this->auto_login();
+		 $teacher = Db::table('tc_teacher')->where('workNumber',$user['workNumber'])->find();
+		 $user = $teacher;
+
+
+
+         return $this->fetch('issueSubmit');
+
+    }
+
+
+    public function studentList() {
+    	$user = $this->auto_login();
+		$teacher = Db::table('tc_teacher')->where('workNumber',$user['workNumber'])->find();
+		$user = $teacher;
+
+
+    	return $this->fetch('studentList');
+
+
+    }
+
+    
+	public function showResult() {
+		$user = $this->auto_login();
+		$teacher = Db::table('tc_teacher')->where('workNumber',$user['workNumber'])->find();
+		$user = $teacher;
+
+
+		return $this->fetch('showResult');
+
 	}
 }
