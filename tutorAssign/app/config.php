987eb66735bc915897d25f2275ba25bdf96ccc2e
weeway <geverway@gmail.com>
2016-11-20 13:47:25 +0800

    change static

--- tutorAssign/app/config.php
+++ tutorAssign/app/config.php
@@ -133,7 +133,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'  =>  [
         '__ROOT__' => __ROOT__,
-        '__STATIC__'=>  "/old",
+        '__STATIC__'=>  OLD,
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
