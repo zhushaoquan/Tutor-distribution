367b383e0c32fc9e847ed48851f39bb5d46b57f1
weeway <geverway@gmail.com>
2016-12-15 22:58:07 +0800

    开始写智能分配界面

---
+++ tutorAssign/old/js/auto_assign.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wythe on 2016/12/15.
+ */
+var vm_table = new Vue({
+    el: "#assign-table",
+    data: {
+        datas: [
+            {
+                serialNum:"031402209",
+                name:"Mike",
+                vol1:"导师",
+                vol2:"导师",
+                vol3:"导师",
+                vol4:"导师",
+                vol5:"导师"
+            }
+        ]
+    }
+});
