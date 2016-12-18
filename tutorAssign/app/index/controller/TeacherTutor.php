3cab828426928740b7df74ae63eaa97bd4c74eca
xuowner <530509150@qq.com>
2016-12-19 00:09:14 +0800

    辣鸡软工

--- tutorAssign/app/index/controller/TeacherTutor.php
+++ tutorAssign/app/index/controller/TeacherTutor.php
@@ -183,12 +183,21 @@ class TeacherTutor extends BaseController {
         exit;
      
     }
+
+  
     public function showNotice($str, $smartMode) {
+
         $str = str_replace("\n", "", $str);
         $data['info'] = $str;
         $data['url'] = $smartMode;
-        return json($data);
-/*
+    echo '<script type="text/javascript" src="__STATIC__/js/index.js"></script>';
+    echo '<script type="text/javascript" src="__STATIC__/js/jquery2.14.min.js"></script>';
+    echo '<script type="text/javascript" src="__STATIC__/js/bootstrap.js"></script>';
+    echo '<script type="text/javascript" src="__STATIC__/js/backstage.js"></script>';
+  
+
+
+      return json($data);
         echo '<DOCTYPE HTML>';
         echo '<html>';
         echo '<head>';
@@ -197,13 +206,55 @@ class TeacherTutor extends BaseController {
         echo '</head>';
         echo '<body>';
         echo '<script language="javascript">';
-        echo "alert('".addslashes($str)."');";
+        echo '<script>window.alert = function(str)
+{
+    var shield = document.createElement("DIV");
+    shield.id = "shield";
+    shield.style.position = "absolute";
+    shield.style.left = "0px";
+    shield.style.top = "0px";
+    shield.style.width = "100%";
+    shield.style.height = document.body.scrollHeight+"px";
+    //弹出对话框时的背景颜色
+    shield.style.background = "#fff";
+    shield.style.textAlign = "center";
+    shield.style.zIndex = "25";
+    //背景透明 IE有效
+    //shield.style.filter = "alpha(opacity=0)";
+    var alertFram = document.createElement("DIV");
+    alertFram.id="alertFram";
+    alertFram.style.position = "absolute";
+    alertFram.style.left = "50%";
+    alertFram.style.top = "50%";
+    alertFram.style.marginLeft = "-225px";
+    alertFram.style.marginTop = "-75px";
+    alertFram.style.width = "450px";
+    alertFram.style.height = "150px";
+    alertFram.style.background = "#ff0000";
+    alertFram.style.textAlign = "center";
+    alertFram.style.lineHeight = "150px";
+    alertFram.style.zIndex = "300";
+    strHtml = "<ul style=\"list-style:none;margin:0px;padding:0px;width:100%\">\n";
+    strHtml += " <li style=\"background:#DD828D;text-align:left;padding-left:20px;font-size:14px;font-weight:bold;height:25px;line-height:25px;border:1px solid #F9CADE;\">[自定义提示]</li>\n";
+    strHtml += " <li style=\"background:#fff;text-align:center;font-size:12px;height:120px;line-height:120px;border-left:1px solid #F9CADE;border-right:1px solid #F9CADE;\">"+str+"</li>\n";
+    strHtml += " <li style=\"background:#FDEEF4;text-align:center;font-weight:bold;height:25px;line-height:25px; border:1px solid #F9CADE;\"><input type=\"button\" value=\"确 定\" onclick=\"doOk()\" /></li>\n";
+    strHtml += "</ul>\n";
+    alertFram.innerHTML = strHtml;
+    document.body.appendChild(alertFram);
+    document.body.appendChild(shield);
+    var ad = setInterval("doAlpha()",5);
+    this.doOk = function(){
+    alertFram.style.display = "none";
+    shield.style.display = "none";
+    }
+    alertFram.focus();
+    document.body.onselectstart = function(){return false;};
+}</script>';
         echo 'window.location.href="'.$smartMode.'";';
         echo '</script>';
         echo '</body>';
         echo '</html>';
-*/
-        exit;
+        
     }
 
      public function issue_submit() {
@@ -290,7 +341,7 @@ class TeacherTutor extends BaseController {
                  $data3['info'] = '课题修改成功';
                  $data3['url'] = url('TeacherTutor/issue_submit');
               //   return json($data3);
-             //   $this->showNotice('课题修改成功', url('TeacherTutor/issue_submit'));      
+                $this->showNotice('课题修改成功', url('TeacherTutor/issue_submit'));      
              } 
              
         }
--- tutorAssign/old/js/student_list.js
+++ tutorAssign/old/js/student_list.js
@@ -20,7 +20,9 @@ refreshout();
 //  加载弹出框里面的信息
 function refreshout(){
 	$("#select1").click(function(){
-		var sid=$(this).parent().children();
+		var sid=$($("#select1").parent().parent().children().get(0)).children().val();
+		console.log(sid);
+		
 		$.ajax({
 			type:"get",
 			data:{
@@ -40,40 +42,6 @@ function refreshout(){
      });
 }
 
-// //弹出框里中选学生的sid
-// function selectsid(){
-// 	var radio = $(".radio");
-// 	if (radio.checked){
-// 		radio.push(radio.sid);
-// 	}
-// 	return radio;
-// }
-
-// //监听选择弹出框事件
-// function listenEventChoice(){
-// 	$("#choiceclose").click(function(){
-// 		location.reload();
-// 	});
-// 	$("#choiceagain").click(function(){
-// 		$("#choiceagain").attr("disabled","disabled");
-// 		var sid=selectsid();
-// 		$.ajax({
-// 			type:"get",
-// 			data:{
-// 				sid:radio
-// 			},
-// 			url:"{{$Think.const.PREFIX}}/TeacherTutor/show_studentdetail",
-// 			success:function(data){
-// 				if(data){
-// 					$("#failed").text("选择失败!").css("color","res");
-// 				}else{
-// 					$("#failed").text("选择成功!").css("color","green");
-// 					$("#choiceagain").attr("disabled","false");
-// 				}
-// 			},
-// 		});
-// 	});
-// }
 
 //vue modal初始化
 var reject_out =new Vue({
