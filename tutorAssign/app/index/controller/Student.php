a5c0d955acdd69f23c56540bedcfb8b570d3b287
tingting <771539035@qq.com>
2016-12-18 20:14:15 +0800

    提示信息修改，测试接口

--- tutorAssign/app/index/controller/Student.php
+++ tutorAssign/app/index/controller/Student.php
@@ -50,7 +50,7 @@ class Student extends BaseController {
 
 	        if($this->user['chosen'] == 1) {
 	        	$data['ontime'] = 3;
-	        	$data['message'] = "<font color='#FF0000'>"$this->user['name']."同学，"."</font>志愿结果已出，请前往 最终结果 页面查看哦~~~";
+	        	$data['message'] = "<font color='#FF0000'>"$this->user['name']."</font>同学，志愿结果已出，请前往 最终结果 页面查看哦~~~";
 	        }else if($nowtime >= $data['issueStart'] && $nowtime <= $data['issueEnd']) {
 	        	//导师填报课题时段！
 	        	$data['ontime'] = 0;
