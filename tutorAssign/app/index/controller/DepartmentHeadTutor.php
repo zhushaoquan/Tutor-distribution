fb1c68c5a5f8288439b3d453fb9837b6a10dbd3f
Liam <282129796@qq.com>
2016-12-19 17:42:16 +0800

    未分配导师接口修改

--- tutorAssign/app/index/controller/DepartmentHeadTutor.php
+++ tutorAssign/app/index/controller/DepartmentHeadTutor.php
@@ -388,7 +388,15 @@ class DepartmentHeadTutor extends BaseController {
 	//确认分配结果
 	public function assignResultConfirm() {
 		$checkedList = Db::table('tc_temp_result')->where('checked',1)->select();
+		$grade = Db::table('tc_grade')->order('grade desc')->select();
 
+		$count = count($checkedList);
+		for ($i=0; $i <$count ; $i++) { 
+			$result['sid'] = $checkedList[$i]['sid'];
+			$result['workNumber'] = $checkedList[$i]['workNumber'];
+
+			Db::table('tc_result_'.$grade[0]['grade']);
+		}
 	}
 
 
@@ -1326,11 +1334,17 @@ class DepartmentHeadTutor extends BaseController {
     public function teacherIssue() {
     	$user = $this->auto_login();
     	$department = $user['department'];
-    	// $user['department'] = "信息安全与网络工程系";
+    	// $user['department'] = "数学实验班";
     	$grade = Db::table('tc_grade')->order('grade desc')->select();
 
-    	$issue['information'] = Db::table('user_teacher t,tc_issue_'.$grade[0]['grade'].' i')->where('t.workNumber=i.workNumber')->where('t.department',$user['department'])->field('t.name as name,i.workNumber as workNumber,t.isExperial as isExperial,i.totalCompExper as js_need,i.compExperNow as js_cur,i.totalMathExper as ss_need,i.mathExperNow as ss_cur,i.totalNatur as nature_need,i.naturNow as nature_cur')->select();
-
+    	if ($department == "计算机实验班") {
+    		$issue['information'] = Db::table('user_teacher t,tc_issue_'.$grade[0]['grade'].' i')->where('t.workNumber=i.workNumber')->where('i.totalCompExper','>','i.compExperNow')->where('t.isExperial',1)->whereOr('t.isExperial',3)->field('t.name as name,i.workNumber as workNumber,t.isExperial as isExperial,i.totalCompExper as js_need,i.compExperNow as js_cur,i.totalMathExper as ss_need,i.mathExperNow as ss_cur,i.totalNatur as nature_need,i.naturNow as nature_cur')->select();
+    	} elseif ($department == "数学实验班") {
+    		$issue['information'] = Db::table('user_teacher t,tc_issue_'.$grade[0]['grade'].' i')->where('t.workNumber=i.workNumber')->where('i.totalMathExper','>','i.mathExperNow')->where('t.isExperial',2)->whereOr('t.isExperial',3)->field('t.name as name,i.workNumber as workNumber,t.isExperial as isExperial,i.totalCompExper as js_need,i.compExperNow as js_cur,i.totalMathExper as ss_need,i.mathExperNow as ss_cur,i.totalNatur as nature_need,i.naturNow as nature_cur')->select();
+    	} else {
+    		$issue['information'] = Db::table('user_teacher t,tc_issue_'.$grade[0]['grade'].' i')->where('t.workNumber=i.workNumber')->where('i.totalNatur','>','i.naturNow')->where('t.department',$user['department'])->field('t.name as name,i.workNumber as workNumber,t.isExperial as isExperial,i.totalCompExper as js_need,i.compExperNow as js_cur,i.totalMathExper as ss_need,i.mathExperNow as ss_cur,i.totalNatur as nature_need,i.naturNow as nature_cur')->select();
+    	}
+    	echo $issue::getLastSql();
     	return json($issue);
     }
 
