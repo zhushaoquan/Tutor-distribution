52d189c7369f5230c115be713834cea5c2215e5e
weeway <geverway@gmail.com>
2017-01-19 19:45:10 +0800

    change index

--- tutorAssign/public/index.php
+++ tutorAssign/public/index.php
@@ -12,14 +12,14 @@ define('APP_DEBUG', false);
 
 //css 前缀
 $_path = explode("/public",$_SERVER['SCRIPT_NAME']);
-define('TUTOR_STATIC',"http://".$_SERVER['HTTP_HOST'].$_path[0].'/static');
-define('OLD',"http://".$_SERVER['HTTP_HOST'].$_path[0].'/old');
-define('STATIC',"http://".$_SERVER['HTTP_HOST'].$_path[0].'/static');
-define('COMMON_PATH',"http://".$_SERVER['HTTP_HOST'].$_path[0]);
+define('TUTOR_STATIC',"https://".$_SERVER['HTTP_HOST'].$_path[0].'/static');
+define('OLD',"https://".$_SERVER['HTTP_HOST'].$_path[0].'/old');
+define('STATIC',"https://".$_SERVER['HTTP_HOST'].$_path[0].'/static');
+define('COMMON_PATH',"https://".$_SERVER['HTTP_HOST'].$_path[0]);
 
 //接口 前缀
 $_prefix = explode('index/',$_SERVER['SCRIPT_NAME']);
-define('PREFIX', "http://".$_SERVER['HTTP_HOST'].$_prefix[0]."/index");
+define('PREFIX', "https://".$_SERVER['HTTP_HOST'].$_prefix[0]."/index");
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
