1c0afd001a9d01b42ce0dfea505efe69f4e4a2c3
weeway <geverway@gmail.com>
2017-03-08 12:11:31 +0800

    modify databasename' “

--- tutorAssign/app/database.php
+++ tutorAssign/app/database.php
@@ -23,7 +23,7 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'tutor_system',
+    'database'       => 'teacher_class_system',
     // 用户名
     'username'       => 'root',
     // 密码
